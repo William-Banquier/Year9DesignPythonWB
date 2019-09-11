e8d40edc026b5dad0a74b10447425f8f2ddae2f1
William Banquier <william.banquier@GCCYJTLQJ1WK.local>
2019-09-11 10:46:22 -0400

    Update

--- Takinginputs.py
+++ Takinginputs.py
@@ -6,4 +6,7 @@ print(input("This is a input: "))
 numbers = input("Numbers: ").split()
 
 #Output (Prints function and then inside of print multipies)
-print(int(numbers[0])*int(numbers[1]))
+#firstly it turns the first and second indexes of numbers into ints
+#then multiplys them
+#lastly it prints the output
+print("The product is",str(int(numbers[0])*int(numbers[1])))
