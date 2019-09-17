38e5b7b6b10984d54157aba57523298dde748633
William Banquier <william.banquier@GCCYJTLQJ1WK.local>
2019-09-17 10:59:58 -0400

    Update w/ digital DNA part one

---
+++ Exercise 6 - DigitalDNA/DigitalDNA.py
@@ -0,0 +1,10 @@
+def findAminoAcid(s):
+	x = 0
+	AA_CODE =["TTT","TTC","TTA","TTG","TCT","TCC","TCA","TCG","TAT","TAC","TAA","TAG","TGT","TGC","TGA","TGG","CTT","CTC","CTA","CTG","CCT","CCC","CCA","CCG","CAT","CAC","CAA","CAG","CGT","CGC","CGA","CGG","ATT","ATC","ATA","ATG","ACT","ACC","ACA","ACG","AAT","AAC","AAA","AAG","AGT","AGC","AGA","AGG","GTT","GTC","GTA","GTG","GCT","GCC","GCA","GCG","GAT","GAC","GAA","GAG","GGT","GGC","GGA","GGG",]
+	AA_CODE_VALUE = ["F","F","L","L","S","S","S","S","Y","Y","STOP","STOP","C","C","STOP","W","L","L","L","L","P","P","P","P","H","H","Q","Q","R","R","R","R","I","I","I","M","T","T","T","T","N","N","K","K","S","S","R","R","V","V","V","V","A","A","A","A","D","D","E","E","G","G","G","G",]
+	while x != len(AA_CODE):
+		if AA_CODE[x] == s:
+			print (AA_CODE_VALUE[x])
+		x+=1
+
+findAminoAcid("TGA")
